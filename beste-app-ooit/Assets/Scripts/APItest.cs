eadbc0e1e3be7e0d4909c2008b90479c19378876
Coolboyfeel <felixmudde.live@gmail.com>
2024-09-26 21:13:10 +0200

    Heel veel, zie description.
    • package van 'microsoft.extension.http' toegevoegd + dependensies
    • Test script toegevoegd
    • Het werkt nog steeds niet :( (maar runt wel)

---
+++ beste-app-ooit/Assets/Scripts/APItest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+
+using UnityEngine;
+using UnityEngine.Networking;
+using ParkSquare.Discogs;
+using Microsoft.Extensions.Http;
+
+public class APItest : MonoBehaviour
+{
+    public string[] test;
+
+    public void Start() {
+        Debug.Log("Start");
+        Main(test);
+    }
+
+    public static void Main(string[] args) {
+        Debug.Log("Main Called");
+        DiscogsClient client = new DiscogsClient( 
+            new HttpClient(new HttpClientHandler()),
+            new ApiQueryBuilder(new HardCodedClientConfig())); 
+
+            var searchRes = client.SearchAsync(new SearchCriteria 
+            {
+                Artist = "Taylor Swift", 
+                ReleaseTitle = "Tortured Poets Department"
+            }).Result;
+            Debug.Log(searchRes);
+    }
+}
+
+public class HardCodedClientConfig : IClientConfig
+    {
+        public string AuthToken => "SuycForINtHDBKeVxxSGLQSsOtkjOeGpCdKBzENj";
+
+        public string BaseUrl => "https://api.discogs.com";
+    }
